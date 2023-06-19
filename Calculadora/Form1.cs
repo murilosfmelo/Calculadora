385dc6caf7266e3b5bb8ab3b7779c79f6ebc41f2
MURILO SABINO FARIAS DE MELO <murilo.sfmelo@senacsp.edu.br>
2023-06-19 14:29:29 -0300

    Colocando alguns botões

--- Calculadora/Form1.cs
+++ Calculadora/Form1.cs
@@ -21,5 +21,80 @@ namespace Calculadora
         {
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TxtResultado.Text = TxtResultado.Text + "8";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            TxtResultado.Text = TxtResultado.Text + "9";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            TxtResultado.Text = TxtResultado.Text + "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            TxtResultado.Text = TxtResultado.Text + "0";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            TxtResultado.Text = TxtResultado.Text + ".";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            TxtResultado.Text = TxtResultado.Text + "1";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            TxtResultado.Text = TxtResultado.Text + "2";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            TxtResultado.Text = TxtResultado.Text + "3";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            TxtResultado.Text = TxtResultado.Text + "5";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            TxtResultado.Text = TxtResultado.Text + "6";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TxtResultado.Text = TxtResultado.Text + "7";
+        }
     }
 }
